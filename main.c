02a86db02856bffde50b2a80438d0d6da4a295a2
Facundo Rotunno <facu.rotunno@gmail.com>
2019-05-03 20:39:20 -0300

    raiz cuadrada

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+void main (){
+    float num;
+    float res;
+
+    printf("Ingrese valor:");
+    scanf("%f", &num);
+
+    res = sqrt (num);
+
+    printf("El resultado es: %f", res);
+
+
+}
